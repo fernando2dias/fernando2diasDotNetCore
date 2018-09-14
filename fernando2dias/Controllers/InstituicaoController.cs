08c66fa43418e42eb901a75632302c446e5990c0
Fernando Dias Motta <fernando3dias@gmail.com>
2018-09-14 14:06:28 -0300

    aula 02

--- fernando2dias/Controllers/InstituicaoController.cs
+++ fernando2dias/Controllers/InstituicaoController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using fernando2dias.Models;
+
 
 namespace fernando2dias.Controllers
 {
     public class InstituicaoController : Controller
     {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao(){InstituicaoID =1, Nome = "UniParaná", Endereco = "Paraná" },
+            new Instituicao(){InstituicaoID =2, Nome = "UniSanta", Endereco = "Santa Catarina" },
+            new Instituicao(){InstituicaoID =3, Nome = "UniSãoPaulo", Endereco = "São Paulo" },
+            new Instituicao(){InstituicaoID =4, Nome = "UniRJ", Endereco = "Rio de Janeiro" },
+            new Instituicao(){InstituicaoID =5, Nome = "UniRS", Endereco = "Rio Grande do Sul" }
+        };
+
         public IActionResult Index()
         {
-            return View();
+            return View(instituicoes);
         }
     }
 }
